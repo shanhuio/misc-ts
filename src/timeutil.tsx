72ebd07000c61ff5c901e512d5b8b93b424ef1d5
Lonnie Liu <liulonnie@gmail.com>
2021-09-20 20:00:57 -0700

    timeutil

--- src/timeutil.tsx
+++ src/timeutil.tsx
@@ -1,11 +1,17 @@
-class Timestamp {
+// Timestamp is a simple data structure that stores the time stamp.
+// It always uses the UTC timezone. This maps to the Timestamp type in
+// Go package shanhu.io/misc/timeutil
+export class Timestamp {
     Sec: number
     Nano: number
 }
 
-function unix(ts: Timestamp) { return ts.Sec }
+// unix returns the Unix UTC timestamp (in seconds).
+export function unix(ts: Timestamp) { return ts.Sec }
 
-class Duration {
+// Duration is a simple data structure that stores a time duration.
+// It maps to the Duration type in Go package shanhu.io/misc/timeutil
+export class Duration {
     Sec: number
     Nano: number
 }
