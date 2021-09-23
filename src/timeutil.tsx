624f4464a8cc5bd310b37d230cf5974508e05063
Lonnie Liu <liulonnie@gmail.com>
2021-09-23 13:32:01 -0700

    add license notice

--- src/timeutil.tsx
+++ src/timeutil.tsx
@@ -1,3 +1,18 @@
+// Copyright (C) 2021  Shanhu Tech Inc.
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // Timestamp is a simple data structure that stores the time stamp.
 // It always uses the UTC timezone. This maps to the Timestamp type in
 // Go package shanhu.io/misc/timeutil
